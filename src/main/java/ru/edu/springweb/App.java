13f12a0d1210eeeefb129e50c9143fee1010c3ce
RodinRoman <roman.v.rodin@gmail.com>
2024-11-23 02:11:02 +0300

    init

---
+++ src/main/java/ru/edu/springweb/App.java
@@ -0,0 +1,13 @@
+package ru.edu.springweb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
